9955d9fe0efa8cb3cbed07b2e9e32e71dd5d5625
saiko233 <94412031+Saiko233@users.noreply.github.com>
2024-07-04 13:25:25 +0900

    footer

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ $(".slider").slick({
   variableWidth: true, //幅の違う画像の高さを揃えて表示
   dots: true, //下部ドットナビゲーションの表示
 });
+
+$(function () {
+  $('a[href^="#"]').click(function () {
+    let speed = 100;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+});
